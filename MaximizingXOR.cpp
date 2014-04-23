6fbbc2a2baa34ad0484904962ab3a8c07d9e5139
Hao Hu <phoenixinter@gmail.com>
2014-04-23 15:11:37 -0700

    Add MaximizingXOR and VolleyballMatch

---
+++ MaximizingXOR.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int l, r;
+	int ans = 0;
+	cin >> l >> r;
+	bool flag = false;
+	for (int i = 30; i >= 0; i--)
+	{
+		int val1 = l & (1 << i);
+		int val2 = r & (1 << i);
+		if (val1 != val2)
+			flag = true;
+		if (flag)
+			ans |= (1 << i);
+	}
+	cout << ans << endl;
+	return 0;
+}
---
+++ VolleyballMatch.cpp
@@ -0,0 +1,57 @@
+#include<iostream>
+using namespace std;
+
+const int MOD = 1000000007;
+
+bool TestValid(int a, int b)
+{
+	if (a < 25) return false;
+	else if (a == 25) return b <= 23;
+	else return b == a - 2;
+}
+
+long long powmod(long long a, int n)
+{
+	long long res = 1;
+	while (n > 0) {
+		if (n & 1) res = (res * a) % MOD;
+		a = (a * a) % MOD;
+		n /= 2;
+	}
+	return res;
+}
+
+long long f[50][50];
+
+void init()
+{
+	for (int i = 0; i < 50; i++)
+		f[i][0] = f[i][i] = 1;
+	for (int i = 2; i < 50; i++)
+		for (int j = 1; j < i; j++)
+			f[i][j] = (f[i - 1][j] + f[i - 1][j - 1]) % MOD;
+}
+
+int main()
+{
+	init();
+	int a, b;
+	cin >> a >> b;
+	if (a < b) swap(a, b);
+	if (!TestValid(a, b))
+		cout << 0 << endl;
+	else
+	{
+		if (a > 25)
+		{
+			int diff = b - 24;
+			long long ans = (powmod(2, diff) * f[48][24]) % MOD;
+			cout << ans << endl;
+		}
+		else
+		{
+			cout << f[a + b - 1][b] << endl;
+		}
+	}
+	return 0;
+}
