9177d0e81a5a42ece97b5cf02f0636eacb6f3f7c
Derek Hao Hu <phoenixinter@gmail.com>
2014-04-25 09:13:08 -0700

    Commit Pairs

---
+++ Pairs.cpp
@@ -0,0 +1,30 @@
+//Pairs.cpp
+//Pairs
+//Algorithms - Search
+//Author: derekhh
+
+#include<iostream>
+#include<set>
+using namespace std;
+
+set<int> s;
+
+int main()
+{
+	int n, k, val;
+	cin >> n >> k;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> val;
+		s.insert(val);
+	}
+	int ans = 0;
+	for (set<int>::iterator it = s.begin(); it != s.end(); ++it)
+	{
+		int val = *it;
+		if (s.find(val + k) != s.end()) ans++;
+		if (s.find(val - k) != s.end()) ans++;
+	}
+	cout << ans / 2 << endl;
+	return 0;
+}
