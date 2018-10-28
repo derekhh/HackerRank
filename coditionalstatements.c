50b470d4d77452cc1f22328c8651b48446994e5f
highskyaj <43993688+highskyaj@users.noreply.github.com>
2018-10-29 00:03:24 +0530

    Create coditionalstatements.c

---
+++ coditionalstatements.c
@@ -0,0 +1,46 @@
+#include <assert.h>
+#include <limits.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+
+
+
+int main()
+{
+   
+
+    // Write Your Code Here
+int z;
+    scanf("%d",&z);
+    if(z==1)
+        printf("one");
+else    if(z==2)
+        printf("two");
+  else  if(z==1)
+        printf("one");
+ else   if(z==3)
+        printf("three");
+  else  if(z==4)
+        printf("four");
+ else   if(z==5)
+        printf("five");
+ else   if(z==6)
+        printf("six");
+  else  if(z==7)
+        printf("seven");
+  else  if(z==8)
+        printf("eight");
+  else  if(z==9)
+        printf("nine");
+else     if(z>9)
+    printf("Greater than 9");
+    return 0;
+}
+
