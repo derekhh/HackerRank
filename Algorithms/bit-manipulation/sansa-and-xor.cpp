9a1b8823e6f3c286db314ab675071f2a5a2b8e7d
Megha Garg <43913641+miss-jain-16@users.noreply.github.com>
2020-10-02 11:11:33 +0530

    Update sansa-and-xor.cpp

--- Algorithms/bit-manipulation/sansa-and-xor.cpp
+++ Algorithms/bit-manipulation/sansa-and-xor.cpp
@@ -11,20 +11,22 @@ int main()
 {
 	int t;
 	cin >> t;
-	while (t--)
-	{
-		int n;
-		cin >> n;
-		int ans = 0;
-		for (int i = 0; i < n; i++)
-		{
-			int num;
-			cin >> num;
-			long long times = (long long)(i + 1) * (n - i);
-			if (times % 2 == 1)
-				ans ^= num;
-		}
-		cout << ans << endl;
-	}
-	return 0;
+	while(t--)
+    {
+        int n,x;
+        cin>>n;
+        as(n,1,100000);
+        int val = 0;
+        for(int i=1 ; i<=n ; i++)
+        {
+            cin>>x;
+            as(x,1,100000000);
+            if(i%2 != 0)
+                val = val^x;
+        }
+        if(n%2 == 0)
+            cout<<0<<endl;
+        else
+            cout<<val<<endl;
+    }
 }
